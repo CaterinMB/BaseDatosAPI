dc10547e9fac3640dada10245938f36fdd3cae4b
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-03-30 10:15:32 -0500

    Update VentasRouter.js

--- Crud/routes/VentasRouter.js
+++ Crud/routes/VentasRouter.js
@@ -1,4 +1,5 @@
 const express = require('express')
+app = express()
 const bodyParser = require('body-parser')
 const { MongoClient, ObjectId} = require("mongodb");
 const uri = "mongodb+srv://Pacho:Pacho12314@pach-os.bim9k0o.mongodb.net/?retryWrites=true&w=majority";
@@ -25,7 +26,7 @@ router.get('/', async(req, res)=>{
 
 // 2 findOne() HTTP: GET
 router.get('/:id', async(req, res)=>{
-    const id = req.params.id_Venta
+    const id = req.params.id
     const client = new MongoClient(uri)
     try{
         await client.connect()
@@ -61,14 +62,32 @@ router.post('/', async(req, res)=>{
 })
 
 // UPDATE
-// UpdateOne() HTTP: PATCH
-router.patch('/:id', async(req, res)=>{
-    const id = req.params.id_Venta
+// UpdateOne() HTTP: PUT
+router.put("/:id", async(req, res) =>{
+    const id = req.params.id
     const body = req.body
     const client = new MongoClient(uri)
     try{
         await client.connect()
-        const result = await client.db('Pach_OS').collection('Ventas').updateOne({_id: new ObjectId(id)}, {$set:{Cliente: body.Cliente, id_Usuario: body.id_Usuario}})
+        const result = await client.db("Pach_OS").collection("Ventas").updateOne({_id: new ObjectId(id)}, {$set: body})
+        res.json({
+            message: "Venta Actualizada",
+            data: body
+        })
+    }finally{
+        await client.close()
+    }
+})
+
+// UPDATE
+// UpdateOne() HTTP: PATCH
+router.patch('/:id', async(req, res)=>{
+    const id = req.params.id
+    const body = req.body
+    const client = new MongoClient(uri)
+    try{
+        await client.connect()
+        const result = await client.db('Pach_OS').collection('Ventas').updateOne({_id: new ObjectId(id)}, {$set:{body}})
         res.status(200).json({
             message: 'Venta Actualizada',
             data: body,
@@ -82,7 +101,7 @@ router.patch('/:id', async(req, res)=>{
 // DELETE
 // DeleteOne() HTTP: DELETE
 router.delete('/:id', async(req, res)=>{
-    const id = req.params.id_Venta
+    const id = req.params.id
     const client = new MongoClient(uri)
     try{
         await client.connect()
